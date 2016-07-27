ff56176e29df0e17877b50f4b8dfa05a0a496df7
曲艺 <quyi1985@gmail.com>
2016-07-27 16:00:36 +0800

    Initial Commit

---
+++ smc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  smc
+//
+//  Created by 曲艺 on 16/7/27.
+//  Copyright © 2016年 曲艺. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
