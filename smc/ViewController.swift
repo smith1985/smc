50b805c532a6e84cc989fa4751b4b19c916d117b
曲艺 <quyi1985@gmail.com>
2016-07-27 17:35:50 +0800

    3333

--- smc/ViewController.swift
+++ smc/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("123")
+        print("666")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
