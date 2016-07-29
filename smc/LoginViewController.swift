9de4dbb965f7f0590bdecbac4d36a0242fecdf51
曲艺 <quyi1985@gmail.com>
2016-07-29 18:21:34 +0800

    1607291821

--- smc/LoginViewController.swift
+++ smc/LoginViewController.swift
@@ -12,6 +12,7 @@ class LoginViewController: UIViewController {
     
     @IBOutlet weak var txt_loginName: UITextField!
     @IBOutlet weak var txt_Pwd: UITextField!
+    @IBOutlet weak var LoginBtn: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -30,20 +31,38 @@ class LoginViewController: UIViewController {
     }
     //登陆
     @IBAction func LoginBtn_Click(sender: AnyObject) {
-        let url="4/news/latest"
-        ZFNetworkTool.get(
-            url
-            , params: nil
-            , success: {
-                (json) -> Void in
-                print(json)
-            }
-            )
-        {
-            //尾随闭包，也就是最后一个参数。
-            (error) -> Void in
-            print(error)
+ 
+        if txt_loginName.text!.isEmpty{
+            
+            let alertController = UIAlertController(title: "提示", message:"请输入用户名", preferredStyle: UIAlertControllerStyle.Alert)
+            alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default,handler: nil))
+            self.presentViewController(alertController, animated: true, completion: nil)
+            return
         }
+        
+        if txt_Pwd.text!.isEmpty{
+            
+            let alertController = UIAlertController(title: "提示", message:"请输入密码", preferredStyle: UIAlertControllerStyle.Alert)
+            alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default,handler: nil))
+            self.presentViewController(alertController, animated: true, completion: nil)
+            return
+        }
+        
+        
+//        let url="4/news/latest"
+//        ZFNetworkTool.get(
+//            url
+//            , params: nil
+//            , success: {
+//                (json) -> Void in
+//                print(json)
+//            }
+//            )
+//        {
+//            //尾随闭包，也就是最后一个参数。
+//            (error) -> Void in
+//            print(error)
+//        }
     }
     
     
@@ -52,8 +71,18 @@ class LoginViewController: UIViewController {
 extension LoginViewController:UITextFieldDelegate{
     
     func textFieldShouldReturn(textField: UITextField) -> Bool {
-        txt_loginName.resignFirstResponder()
-        txt_Pwd.resignFirstResponder()
+//        txt_loginName.resignFirstResponder()
+//        txt_Pwd.resignFirstResponder()
+        
+        if textField==self.txt_loginName{
+            
+            txt_loginName.resignFirstResponder()
+            txt_Pwd.becomeFirstResponder()
+        }
+        else{
+            txt_Pwd.resignFirstResponder()
+            LoginBtn_Click(self.LoginBtn)
+        }
         return true
     }
 }
